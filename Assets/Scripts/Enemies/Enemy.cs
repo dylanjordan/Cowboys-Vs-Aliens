f95f56f38931c0f36c11360953ab50ca48623403
Dylan Jordan <dylanpeterjordan@gmail.com>
2022-03-23 02:13:40 -0400

    finished Enemy Script & Touch Ups For Presentation

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] TestHealthbar _healthBarControl;
-    
+
     [SerializeField] GameObject _healthBar;
-    
-    [SerializeField]  float _deathTimer = 0.3f;
+
+    [SerializeField] float _deathTimer = 0.3f;
 
     Rigidbody2D _rb;
 
@@ -37,7 +37,11 @@ public class Enemy : MonoBehaviour
     {
         ResetHealth();
     }
-
+    private void Update()
+    {
+        UpdateEnemy();
+        Move();
+    }
     private void Move()
     {
         if (_movingLeft)
@@ -68,7 +72,7 @@ public class Enemy : MonoBehaviour
     {
         if (CheckIfDead())
         {
-            Destroy(gameObject, _deathTimer);
+            Destroy(gameObject);
         }
     }
     public bool CheckIfDead()
@@ -95,28 +99,6 @@ public class Enemy : MonoBehaviour
         if (collision.gameObject.tag == "Wall")
         {
             _enemySpeed = _enemySpeed * -1;
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.collider.tag == "Player")
-        {
-            _rb.velocity = new Vector2(0, _rb.velocity.y);
-        }
-
-        if (collision.collider.tag == "Gun")
-        {
-            _rb.AddForce(transform.up * 10, ForceMode2D.Impulse);
-            _rb.AddForce(transform.right * 10, ForceMode2D.Impulse);
-
-            _timeSinceReveal += Time.deltaTime;
-            if (_timeSinceReveal >= _timer)
-            {
-                _timeSinceReveal = 0.0f;
-                _healthBar.SetActive(false);
-
-            }
         }
     }
 }
