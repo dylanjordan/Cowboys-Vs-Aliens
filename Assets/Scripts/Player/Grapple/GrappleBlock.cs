df346fbd32278c8390d3ba587f6f60f758271399
William Heath <william.heath@ontariotechu.net>
2022-04-01 14:20:51 -0400

    Grappling has ropes

--- Assets/Scripts/Player/Grapple/GrappleBlock.cs
+++ Assets/Scripts/Player/Grapple/GrappleBlock.cs
@@ -13,11 +13,14 @@ public class GrappleBlock : MonoBehaviour
 
     public GameObject player;
 
+    public LineRenderer _lineRenderer;
+
     public void Start()
     {
         playerSpring = GameObject.FindGameObjectWithTag("Player").GetComponent<SpringJoint2D>();
         player = GameObject.FindGameObjectWithTag("Player");
         block = GetComponent<SpriteRenderer>();
+        _lineRenderer.enabled = false;
         playerSpring.enabled = false;
     }
 
@@ -25,10 +28,17 @@ public class GrappleBlock : MonoBehaviour
     {
         if (playerSpring.enabled)
         {
+            _lineRenderer.SetPosition(0, player.transform.position);
+            _lineRenderer.SetPosition(1, gameObject.transform.position);
             if (Input.GetMouseButtonUp(1))
             {
                 playerSpring.enabled = false;
+                _lineRenderer.enabled = false;
             }
+        }
+        if (!playerSpring.enabled)
+        {
+            _lineRenderer.enabled = false;
         }
     }
 
@@ -38,15 +48,18 @@ public class GrappleBlock : MonoBehaviour
         if (Input.GetMouseButtonDown(1))
         {
             playerSpring.enabled = true;
+            _lineRenderer.enabled = true;
             playerSpring.distance = Vector2.Distance(gameObject.transform.position, player.transform.position);
             playerSpring.connectedBody = GetComponent<Rigidbody2D>();
         }
         if (Input.GetMouseButtonUp(1))
         {
+            _lineRenderer.enabled = false;
             playerSpring.enabled = false;
         }
         if (playerSpring.distance >= 5)
         {
+            _lineRenderer.enabled = false;
             playerSpring.enabled = false;
         }
     }
