92d854d27c12e01902889c4786327576aa04cdfe
William Heath <william.heath@ontariotechu.net>
2022-04-13 00:45:42 -0400

    some audio and some animation fixes

--- Assets/Scripts/Enemies/SpaceShip/SpaceShipAI.cs
+++ Assets/Scripts/Enemies/SpaceShip/SpaceShipAI.cs
@@ -28,6 +28,9 @@ public class SpaceShipAI : MonoBehaviour
     public float _shootRange, _shootSpeed;
     public float _timeBeforeNextShot = 1.0f;
     public float _contactDamage = 1.0f;
+    public float _rotation;
+
+    public AudioClip shootAudio;
 
     [SerializeField] public float _UpdatePathRate = .5f;
     [SerializeField] public float _distanceVal = 5.0f;
@@ -44,6 +47,8 @@ public class SpaceShipAI : MonoBehaviour
     private void Update()
     {
         animator.SetBool("IsShooting", _isInShootingRange);
+        Vector3 angle = _playerLocation.transform.position - shootPos.transform.position;
+        _rotation = Mathf.Atan2(angle.y, angle.x) * Mathf.Rad2Deg;
     }
 
     private void UpdatePath()
@@ -129,8 +134,10 @@ public class SpaceShipAI : MonoBehaviour
         canShoot = false;
         yield return new WaitForSeconds(_timeBeforeNextShot);
         GameObject newBullet = Instantiate(_bulletPrefab, transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+        AudioSource.PlayClipAtPoint(shootAudio, transform.position);
 
         newBullet.GetComponent<Rigidbody2D>().velocity = (_playerModel.position - transform.position).normalized * _shootSpeed;
+        newBullet.transform.rotation = Quaternion.Euler(0.0f, 0.0f, _rotation);
         canShoot = true;
     }
 
