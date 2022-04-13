c7e3e908f72011893dfe01896d9a4630c00326f5
William Heath <william.heath@ontariotechu.net>
2022-04-13 00:48:17 -0400

    coin noise

--- Assets/Scripts/Coin/CoinInteract.cs
+++ Assets/Scripts/Coin/CoinInteract.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class CoinInteract : MonoBehaviour
 {
+    public AudioClip coinSound;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             CoinCounter.coinAmount++;
+            AudioSource.PlayClipAtPoint(coinSound, transform.position);
             Destroy(gameObject);
             PlayerPrefs.SetInt("coinAmount", CoinCounter.coinAmount);
         }
