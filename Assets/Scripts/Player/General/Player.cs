498ab8af4965cb3f7309cbabc348bf2e2e743f77
William Heath <william.heath@ontariotechu.net>
2022-04-13 03:48:42 -0400

    you can die congrats

--- Assets/Scripts/Player/General/Player.cs
+++ Assets/Scripts/Player/General/Player.cs
@@ -13,7 +13,7 @@ public class Player : MonoBehaviour
     public HealthbarUI healthBar;
 
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
         _currentHealth = _maxHealth;
         healthBar.SetMaxHealth(_currentHealth);
@@ -30,8 +30,8 @@ public class Player : MonoBehaviour
     {
         if (_currentHealth <= 0)
         {
-            Destroy(gameObject);
             SceneManager.LoadScene("GameOver");
+            Destroy(gameObject);
         }
     }
 
@@ -40,23 +40,5 @@ public class Player : MonoBehaviour
         _currentHealth -= damage;
         healthBar.SetHealth(_currentHealth);
         Debug.Log("Player's current health is " + _currentHealth);
-    }
-
-    public void SavePlayer()
-    {
-        SaveSystem.SavePlayer(this);
-    }
-
-    public void LoadPlayer()
-    {
-        PlayerData data = SaveSystem.LoadPlayer();
-        _currentHealth = data.health;
-        CoinCounter.coinAmount = data.coins;
-        FindObjectOfType<PlayerMovement>().maxSpeed = data.newSpeed;
-
-        Vector3 position;
-        position.x = data.position[0];
-        position.y = data.position[1];
-        position.z = data.position[2];
     }
 }
